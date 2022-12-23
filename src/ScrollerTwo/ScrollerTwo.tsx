2e865e0b7d1197ef0aeac75ca1cae3666dda4fa4
Yasin Khan <yasinkhan500@gmail.com>
2022-12-22 17:31:56 -0800

    stache

--- src/ScrollerTwo/ScrollerTwo.tsx
+++ src/ScrollerTwo/ScrollerTwo.tsx
@@ -1,16 +1,43 @@
+import { useRef } from 'react';
 import Card from './Card';
 import { scrollerTwoData } from './scrollerTwoData';
-import { FaArrowLeft, FaArrowRight } from 'react-icons/fa';
 
 const ScrollerTwo = () => {
+	const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+	const handleScrollLeft = () => {
+		if (scrollContainerRef.current) {
+			scrollContainerRef.current.scrollTo({
+				left: scrollContainerRef.current.scrollLeft - 300,
+				behavior: 'smooth',
+			});
+		}
+	};
+
+	const handleScrollRight = () => {
+		if (scrollContainerRef.current) {
+			scrollContainerRef.current.scrollTo({
+				left: scrollContainerRef.current.scrollLeft + 300,
+				behavior: 'smooth',
+			});
+		}
+	};
+
 	return (
-		<div className='flex flex-col bg-white m-auto p-auto'>
-			<div className='flex overflow-x-scroll pb-10 no-scroll-bar'>
-				<div className='flex flex-nowrap lg:ml-40 md:ml-20 ml-10 '>
-					{scrollerTwoData.map((item, idx) => (
-						<Card key={item.id} item={item} />
-					))}
+		<div className='flex flex-col bg-white m-auto p-auto relative'>
+			<div className='flex'>
+				<button onClick={handleScrollLeft}>&lt;</button>
+				<div
+					className='overflow-x-scroll pb-10 no-scroll-bar'
+					ref={scrollContainerRef}
+				>
+					<div className='flex flex-nowrap lg:ml-40 md:ml-20 ml-10'>
+						{scrollerTwoData.map(item => (
+							<Card key={item.id} item={item} />
+						))}
+					</div>
 				</div>
+				<button onClick={handleScrollRight}>&gt;</button>
 			</div>
 		</div>
 	);
